51e1ed14db58ff1d588d7512ded0a34b7d4ad288
Oshqovoq bormi <shohjaxonorinboyev8@gmail.com>
2026-09-05 16:24:05 +0500

    Add files via upload

---
+++ Yunusbek.py
@@ -0,0 +1,73 @@
+#kun = int(input("1-7 gacha son kiriting: "))
+
+#match kun:
+   # case 1:
+   #     print("Dushanba")
+   # case 2:
+   #     print("Seshanba")
+   # case 3:
+   #     print("Chorshanba")
+   # case 4:
+   #     print("Payshanba")
+   # case 5:
+   #     print("Juma")
+   # case 6:
+  #      print("Shanba")
+   # case 7:
+   #     print("Yakshanba")
+    #case _:
+     #   print("Bunday hafta kuni mavjud emas")
+
+"""oy_fasli = int(input("Oy raqamini kiriting (1-12): "))
+
+match oy_fasli:
+    case 12 | 1 | 2:
+        print("Qish fasli")
+    case 3 | 4 | 5:
+        print("Bahor fasli")
+    case 6 | 7 | 8:
+        print("Yoz fasli")
+    case 9 | 10 | 11:
+        print("Kuz fasli")
+    case _:
+        print("Bunday oy mavjud emas")"""
+
+"""oylar = ["Yanvar", "Fevral", "Mart", "Aprel", "May", "Iyun",
+         "Iyul", "Avgust", "Sentabr", "Oktabr", "Noyabr", "Dekabr"]
+
+for oy in oylar:
+    print(oy)"""
+
+"""oy = int(input("1-12 gacha son kiriting: "))
+
+match oy:
+    case 1:
+        print("Yanvar")
+    case 2:
+        print("Fevral")
+    case 3:
+        print("Mart")
+    case 4:
+        print("Aprel")
+    case 5:
+        print("May")
+    case 6:
+        print("Iyun")
+    case 7:
+        print("Iyul")
+    case 8:
+        print("Avgust")
+    case 9:
+        print("Sentabr")
+    case 10:
+        print("Oktabr")
+    case 11:
+        print("Noyabr")
+    case 12:
+        print("Dekabr")
+    case _:
+        print("Bunday oy kunlari mavjud emas")"""
+ismlar =("muhamadrizo", "Sobirxoja", "doniyor")
+for ism in(ismlar:
+           
+        
